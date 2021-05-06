a4191a2abb186121e2158699e72ae7f90f0e7463
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2021-05-06 14:10:02 +0700

    add vendor to .gitignore

--- environments/index.php
+++ environments/index.php
@@ -32,37 +32,37 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
+            'admin/runtime',
+            'admin/web/assets',
             'console/runtime',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'client/runtime',
+            'client/web/assets',
         ],
         'setExecutable' => [
             'yii',
             'yii_test',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
+            'admin/config/main-local.php',
             'common/config/codeception-local.php',
-            'frontend/config/main-local.php',
+            'client/config/main-local.php',
         ],
     ],
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
+            'admin/runtime',
+            'admin/web/assets',
             'console/runtime',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'client/runtime',
+            'client/web/assets',
         ],
         'setExecutable' => [
             'yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'admin/config/main-local.php',
+            'client/config/main-local.php',
         ],
     ],
 ];
